e3150aa6109a8d3b9849e910d1ff0307e1f1380b
Aman Kumar Upadhyay <aman08023010@gmail.com>
2024-04-15 23:14:14 +0530

    Reverse string

---
+++ Leet15.cpp
@@ -0,0 +1,15 @@
+/*
+Reverse String
+*/
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size();
+        int i=0;
+        int e=n-1;
+        while(i<e){
+            swap(s[i++],s[e--]);
+        }
+        
+    }
+};
