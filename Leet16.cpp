eb9edb36742175748a58acb64020153e864edcbc
Aman Kumar Upadhyay <aman08023010@gmail.com>
2024-04-17 10:13:24 +0530

    valid palindrome(withaout any prebuilt function)

--- Leet16.cpp
+++ Leet16.cpp
@@ -56,3 +56,57 @@ char toLowerCase(char ch){
         
     }
 };
+
+// withaout using any prebuilt function 
+
+class Solution {
+public:
+
+// function to convert uppercase to lowercase and replace the extra non alphanumeric character with empty character
+        char toLowerCase(char ch){
+            if(ch>='a' && ch<='z'){
+                return ch;
+            }else if(ch>='0' && ch<='9'){
+                char temp=ch - '1' + '1';
+                return temp;
+            }else if(ch>='A' && ch<='Z'){
+                char ctemp=ch - 'A' + 'a';
+                return ctemp;
+            }else{
+                return ' ';
+            }
+            
+        }
+
+
+    bool isPalindrome(string s) {
+        int n=s.length();
+        int k=0;
+        int i=0;
+        char chr[n];
+        for(int j=0;j<n;j++){
+            if (toLowerCase(s[j])!=' ') {
+                chr[k] = toLowerCase(s[j]);
+                k++;
+            }
+        }
+        int e=k-1;
+        if(k<=1){
+            return 1;
+        }else{
+            while(i<=e){
+                
+                if(chr[i]!=chr[e]){
+                    return 0;
+                }else{
+                    i++;
+                    e--;
+                }
+            }
+
+        }
+          
+        return 1;
+        
+    }
+};
