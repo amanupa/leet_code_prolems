a6cabb0cef21194f699e94af0440e31a8aefd669
Aman Kumar Upadhyay <aman08023010@gmail.com>
2024-04-28 23:01:26 +0530

    leeetcode problem

---
+++ Leet28.cpp
@@ -0,0 +1,14 @@
+/*
+*/
+class Solution {
+public:
+    vector<int> minOperations(string boxes) {
+	int len=boxes.size();
+	vector<int> minOps(len,0);
+	for(int i=0; i<len; i++)
+		for(int j=0; j<len; j++)
+			if(boxes[j]=='1')
+				minOps[i]+=abs(i-j);
+	return minOps;
+}
+};
